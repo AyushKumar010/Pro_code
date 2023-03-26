35d8d376941cf1ff94121405962825623a19be50
Ayush Kumar <ayush.kumar06@sap.com>
2023-03-26 10:56:11 +0530

    commit from releaseBranch

--- UserController.js
+++ UserController.js
@@ -2,7 +2,7 @@ const registerUser = (req, res) => {
   const userName = req.body.name;
   const userEmail = req.body.email;
   const userPassword = req.body.password;
-
+// Testing
   res.json({
     success: true,
   });
